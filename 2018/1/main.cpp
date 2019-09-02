3ef5dc4d4e775ab69a8fde2d0ed2daa717ccad24
Armand <ag9217@ic.ac.uk>
2019-09-02 16:25:15 +0200

    refactored day 1 p1

--- 2018/1/main.cpp
+++ 2018/1/main.cpp
@@ -3,16 +3,38 @@
 #include<string>
 #include<vector>
 
+using namespace std;
+
 int main(int argc, const char** argv) {
 
-    std::string inputNum;
-    std::vector<int> log;
-    int sum;
+    //--------------------PART 1--------------------//
+
+    //vectors 
+    std::vector<int> changes; //vector containing all frequency changes
+
+    //variables
+    int inputNum;
+    int freq = 0;
+    bool dupFound = 0;
+
+    //opening input file
     std::ifstream file("input.txt", std::ifstream::in);
     
-    while(getline(file, inputNum)) {
-        sum += std::stoi(inputNum);
-        log.push_back(sum);
+    //pushing all numbers in input file into vector
+    while(!file.eof()) {
+       file >> inputNum;
+       changes.push_back(inputNum);
     }
-    std::cout << sum << std::endl;
+    changes.pop_back(); //last number is put in vector twice
+
+    //calculating sum of frequencies
+    for(int i = 0; i < changes.size(); i++)
+    {
+        freq += changes[i];
+    }
+
+    cout << freq << endl; //printing final frequency
+
+    //--------------------PART 2--------------------//
+    
 }
