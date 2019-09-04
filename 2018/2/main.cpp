675278f68f9e0a9d0358b7b5800299e753259aa6
Armand <ag9217@ic.ac.uk>
2019-09-04 19:38:30 +0200

    initial commit d2

--- 2018/2/main.cpp
+++ 2018/2/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+using namespace std;
 
 int main(int argc, const char** argv) {
-    //something
+
+    std::ifstream file("input.txt", std::ifstream::in); //input file
+
+    vector<string> boxIDs;
+    map<char, int> letFreq;
+    char temp;
+    int tempMax = 0;
+    int tempIndex = 0;
+    string tempBoxID;
+    int i = 2;
+
+    while(file >> tempBoxID) {
+        boxIDs.push_back(tempBoxID);
+    }
+
+    //looking at each string in vector
+    for(int j = 0; j < boxIDs.size(); j++) {
+
+        //check letter frequency in string
+        for(int k = 0; k < boxIDs[j].length(); k++) {
+            temp = boxIDs[j].at(k);
+            std::map<char,int>::iterator it = letFreq.find(temp);
+
+            if(it != letFreq.end()) {
+                it->second++;
+            }
+
+            else {
+                letFreq.insert(make_pair(temp, 1));
+            }
+        }
+    }
 }
