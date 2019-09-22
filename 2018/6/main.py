33191c6ba73d4f0a787fe6b58aa69c82973a758f
Armand <ag9217@ic.ac.uk>
2019-09-22 18:42:22 +0200

    putting coords into grid

--- 2018/6/main.py
+++ 2018/6/main.py
@@ -1,3 +1,10 @@
-coord = open('test.txt').read()
+coords = open('test.txt').read().splitlines()
+
 gridSize = 10
 grid = [['.' for j in range(gridSize)] for i in range(gridSize)]
+
+for coord in coords:
+    xy = coord.split(',')
+    grid[int(xy[0][0])][int(xy[1][1:])] = 'X'
+
+print(grid)
