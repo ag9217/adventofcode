a6df4537476d7d11e7b996bdcb52b935320f5ac5
Armand <ag9217@ic.ac.uk>
2019-09-22 23:32:26 +0200

    added letters for each coord

--- 2018/6/main.py
+++ 2018/6/main.py
@@ -1,10 +1,11 @@
+import string
 coords = open('test.txt').read().splitlines()
 
 gridSize = 10
 grid = [['.' for j in range(gridSize)] for i in range(gridSize)]
 
+i = 0
 for coord in coords:
     xy = coord.split(',')
-    grid[int(xy[0][0])][int(xy[1][1:])] = 'X'
-
-print(grid)
+    grid[int(xy[0][0])][int(xy[1][1:])] = string.ascii_letters[i]
+    i += 1
